974dc8c4fa6eb52f0117428011c3a5bed9af99a5
Cameron <cameron.scott.brown@gmail.com>
2018-01-27 15:09:07 -0800

    added a map, with necessary resources

--- assets/maps/world_map_tiles.tsx
+++ assets/maps/world_map_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="world_map_tiles" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="212" columns="4">
- <image source="../tilesets/world_map/world_map/world_map_tiles.png" width="69" height="902"/>
+ <image source="../tilesets/world_map/world_map_tiles.png" width="69" height="902"/>
 </tileset>
---
+++ assets/tilesets/toens_medieval_tileset/Tile-set - Toen's Medieval Strategy (16x16) - v.1.0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tile-set - Toen's Medieval Strategy (16x16) - v.1.0" tilewidth="16" tileheight="16" tilecount="364" columns="7">
+ <image source="../Toen's Medieval Strategy Sprite Pack v.1.0  (16x16)/Tile-set - Toen's Medieval Strategy (16x16) - v.1.0.png" width="112" height="832"/>
+</tileset>
