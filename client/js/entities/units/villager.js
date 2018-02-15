1d15afc6fc8a526335c2b64f92dd7fcddaeb1505
Juan <duju@oregonstate.edu>
2018-02-14 21:57:47 -0600

    Added A* search for path finding

--- client/js/entities/units/villager.js
+++ client/js/entities/units/villager.js
@@ -3,6 +3,8 @@ import {
   game
 } from '../../game.js';
 
+import {AStar} from '../../astar.js'
+
 import Unit from './unit.js';
 let name = 'villager',
   pixelWidth = 21,
@@ -69,13 +71,70 @@ let name = 'villager',
 						this.orders = {type: "stand"};
 					}
 					else {
-						this.moveTo(this.orders.to, distanceFromDestination);
-						// console.log(this.directions);
+						let moving = this.moveTo(this.orders.to, distanceFromDestination);
+
+            if (!moving){
+              this.orders = {type: "stand"};
+            }
 					}
 					break;
 			}
 
     },
+
+    moveTo: function(destination, distanceFromDestination){
+      let start = [Math.floor(this.x), Math.floor(this.y)];
+      let end = [Math.floor(destination.x), Math.floor(destination.y)];
+
+      let newDirection;
+      let villagerOutsideMapBounds = (start[0] < 0 || start[0] >=game.currentMap.mapGridWidth || start[1] < 0 || start[1] >= game.currentMap.mapGridHeight);
+
+      if (!game.currentMapPassableGrid){
+        game.rebuildPassableGrid();
+      }
+
+      let villagerReachedDestinationTile = (start[0] == destination[0]) && (start[0] == destination[0]);
+
+      if (villagerOutsideMapBounds || villagerReachedDestinationTile){
+        newDirection = this.findAngle(destination);
+        this.orders.path = [[this.x,this.y],[destination.x, destination.y]];
+      }
+      else {
+        let grid;
+        if (destination.type == "buildings" || destination.type == "terrain"){
+          grid = game.makeArrayCopy(game.currentMapPassableGrid);
+
+          grid[Math.floor(destination.x), Math.floor(destination.y)] = 0;
+        }
+        else {
+          grid = game.currentMapPassableGrid;
+        }
+        this.orders.path = AStar(grid,start,end, "Euclidean");
+        //console.log(this.orders.path);
+        if (this.orders.path.length > 1){
+          let nextStep = {x: this.orders.path[1][0]+0.5, y: this.orders.path[1][1]+0.5};
+
+          newDirection = this.findAngle(nextStep);
+        } else {
+          return false;
+        }
+
+        this.turnTo(newDirection);
+        let maximumMovement = this.moveSpeed * (this.turning ? this.speedAdjustmentWhileTurningFactor:1);
+    		let movement = Math.min(distanceFromDestination,maximumMovement);
+
+    		let angleRadians = -(this.direction / this.directions) * 2 * Math.PI;
+
+    		this.lastMovementX = -(movement * Math.sin(angleRadians));
+    		this.lastMovementY = -(movement * Math.cos(angleRadians));
+    		// console.log(newDirection);
+    		this.x = this.x + this.lastMovementX;
+    		this.y = this.y + this.lastMovementY;
+
+        return true;
+      }
+
+    }
   };
 
 let villager = new Unit(name, pixelWidth, pixelHeight, baseWidth, baseHeight, pixelOffsetX,
